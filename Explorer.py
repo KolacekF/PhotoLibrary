1b45166b51ef5a99218720d0111a90f28d3f0b0c
Frantisek Kolacek <ferdakolacek@gmail>
2024-11-05 22:27:16 +0100

    added main.py feature

---
+++ Explorer.py
@@ -0,0 +1,72 @@
+import os
+import pathlib
+
+class Explorer:
+    def __init__(self):
+        self.dot = "."
+        self.workingPath = []
+
+    #accepts string, name of subfolder, which to go in
+    def downLevel(self, l):
+        self.workingPath.append(l)
+
+    #.pop() last record from workingPath = go up one leve
+    def upLevel(self):
+        self.workingPath.pop()
+
+    #return UNIX formated path string
+    def path(self):
+        pathString = self.dot
+        for l in self.workingPath:
+            pathString += f"/{l}"
+        return pathString
+    
+    #scan current directory, return dictionary of folders
+    def scan(self):
+        fileTree = {}
+        files = next(os.walk(self.path()))[1]
+        i = 1
+        for filename in files:
+            fileTree[i] = filename
+            i += 1
+        return fileTree
+    
+    #print() commands, current directory tree and dictionary of scaned files
+    def start(self):
+        print("______________________________")
+        print(self.path())
+        print("Q for quit; S for start; U for one level up")
+        files = self.scan()
+        print(files)
+        x = input() #x is string
+        if x == "Q": #I have to old python for match - case
+            return 0
+        if x == "S":
+            return self.path()
+        elif x == "U":
+            self.upLevel()
+            return self.start()
+        else:
+            self.downLevel(files[int(x)])
+            return self.start()
+        
+
+class ExplorerNew(Explorer):
+    def __init__(self, scriptPath):
+        super().__init__()
+        self.absolutepath = pathlib.Path(scriptPath).parent
+
+    def downLevel(self, l):
+        self.absolutepath = self.absolutepath / l
+
+    def upLevel(self):
+        self.absolutepath = self.absolutepath.parent
+
+    def path(self):
+        return str(self.absolutepath)
+
+
+
+
+#e = Explorer()
+#print(e.start())
---
+++ main.py
@@ -0,0 +1,88 @@
+from Explorer import Explorer, ExplorerNew
+from index_photos import Index_photos
+from create_html import Create_HTML
+import pathlib
+import shutil
+
+
+import os
+
+
+
+#e = Explorer()
+#e = ExplorerNew(__file__)
+#print(next(os.walk(e.path())))     #os.walk(self.path())
+#print(e.start())
+
+def integrityTests():
+    test = True
+    files = ["create_html.py", "index_photos.py", "Explorer.py", "index_TEMPLATE.html", "photo_index_TEMPLATE.db"] #necessary files    
+
+    for f in files:
+        if testFileExists(basePath, f) == False: test = False
+
+    return test
+
+
+
+def main():
+    e = ExplorerNew(__file__) #returns string absolute path
+    setPath = ""
+    
+    print("______________________________")
+    print("Welcome in setup. Do you want to create new database, or update current?")
+    if query("N", "New", "U", "Update") == "N":
+        input(f"in next step, choose path, where the base folder is. Press any key to continue..")
+        setPath = e.start()
+        if testFileExists(setPath, html, False): #test if index.html already exists (it should not when creating new)
+            print(f"in choosen `{html}` already exist, didn`t you want to Update? Do you want to continue?")
+            if query("C", "Continue", "Q", "Quit") == "Q": #else quit because .html file already exists
+                return  
+        
+        shutil.copy(pathlib.Path(basePath) / "photo_index_TEMPLATE.db", pathlib.Path(setPath) / db)
+    else:
+        input(f"in next step, choose path, in which `{db}` and `{html}` are saved. Press any key to continue..")
+        setPath = e.start()
+
+    testFileExists(setPath, db) #test if .db file exists in given path
+
+    #INDEX_PHOTOS.PY
+    a = Index_photos(setPath, db)
+
+    #CREATE_HTML.PY
+    Create_HTML(pathlib.Path(setPath) / db, html, basePath)
+
+
+
+def query(a, aString, b, bString):
+    answer = input(str(a) + " " + aString + ";   " + str(b) + " " + bString + " : ")
+    if answer == a:
+        return a
+    elif answer == b:
+        return b
+    else:
+        return query(a, aString, b, bString)
+    
+def testFileExists(path, file, verbose = True):
+    #file = "photo_index.db"
+
+    if (pathlib.Path(path) / file).is_file():
+        return True
+    else:
+        if verbose: print(f"there was no `{file}` found in `{path}`. Please fix the problem. The script will now Quit.")
+        return False
+
+
+
+if __name__ == "__main__":
+    #ABSOLUTELY DEFINED VARIABLES
+    html = "index.html" #default name of index.html
+    db = "photo_index.db" #default name of database
+    #AUTOMATICALLY DEFINED VARIABLES
+    basePath = pathlib.Path(__file__).parent
+
+
+    if integrityTests():
+        main()
+    else:
+        print("SOME FILES ARE MISSING! The script will now end.")
